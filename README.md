973e84c0c7f63fa40e6c82fd94aca8c0b0aa5069
astesio <astesiojose@gmail.com>
2022-02-02 18:10:07 -0300

    chore: typescript configuration

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 ### ARTICLES
 
-- [GIT COMMIT MESSAGES](https://remarkablemark.org/blog/2019/05/29/git-husky-commitlint/)
+- [GIT COMMIT MESSAGES](https://remarkablemark.org/blog/2019/05/29/git-husky-commitlint)
+- [TYPESCRIPT SERVERLESS](https://codetain.com/blog/lambda-in-typescript-with-serverless-framework)
---
+++ handler.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export const hello = async (event: APIGatewayProxyEvent): Promise<LambdaResponse> => {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  }
+};
+
+export type LambdaResponse = {
+  headers: HeaderOption;
+  statusCode: number;
+  body: string;
+};
+
+interface HeaderOption {
+  'Access-Control-Allow-Origin': string;
+};
